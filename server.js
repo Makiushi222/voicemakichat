894ad03c7b4f209e91e90aff6a048afe89a661f8
Makiushi222 <makiushi434@gmail.com>
2026-05-18 20:30:12 +0200

    server.js

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const http = require("http");
 const { Server } = require("socket.io");
 
 const app = express();
+
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: { origin: "*" }
+    cors: {
+        origin: "*"
+    }
 });
 
 app.use(express.static("."));
@@ -14,40 +17,59 @@ app.use(express.static("."));
 let waitingUser = null;
 
 io.on("connection", (socket) => {
-    console.log("User:", socket.id);
+
+    console.log("Connected:", socket.id);
 
     socket.on("findMatch", () => {
+
         if (waitingUser && waitingUser.id !== socket.id) {
 
-            const roomId = "room-" + waitingUser.id + "-" + socket.id;
+            const roomId =
+                "room-" + waitingUser.id + "-" + socket.id;
 
             socket.join(roomId);
             waitingUser.join(roomId);
 
-            socket.emit("matched", roomId);
-            waitingUser.emit("matched", roomId);
+            // FIRST USER = caller
+            waitingUser.emit("matched", {
+                roomId,
+                isCaller: true
+            });
+
+            // SECOND USER
+            socket.emit("matched", {
+                roomId,
+                isCaller: false
+            });
 
             waitingUser = null;
 
         } else {
+
             waitingUser = socket;
-            socket.emit("waiting");
         }
     });
 
     socket.on("offer", (data) => {
-        socket.to(data.roomId).emit("offer", data.offer);
+
+        socket.to(data.roomId)
+            .emit("offer", data.offer);
     });
 
     socket.on("answer", (data) => {
-        socket.to(data.roomId).emit("answer", data.answer);
+
+        socket.to(data.roomId)
+            .emit("answer", data.answer);
     });
 
     socket.on("ice-candidate", (data) => {
-        socket.to(data.roomId).emit("ice-candidate", data.candidate);
+
+        socket.to(data.roomId)
+            .emit("ice-candidate", data.candidate);
     });
 });
 
 server.listen(process.env.PORT || 3000, () => {
+
     console.log("Server running");
 });
