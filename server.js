bc37d416e4448bf7852d123fe2e85ef6d1d52b3e
Makiushi222 <makiushi434@gmail.com>
2026-05-18 21:10:22 +0200

    server.js

--- server.js
+++ server.js
@@ -3,13 +3,10 @@ const http = require("http");
 const { Server } = require("socket.io");
 
 const app = express();
-
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: "*"
-    }
+    cors: { origin: "*" }
 });
 
 app.use(express.static("."));
@@ -20,7 +17,6 @@ io.on("connection", (socket) => {
 
     console.log("Connected:", socket.id);
 
-    // 🔍 Find Match
     socket.on("findMatch", () => {
 
         if (waitingUser && waitingUser.id !== socket.id) {
@@ -31,13 +27,11 @@ io.on("connection", (socket) => {
             socket.join(roomId);
             waitingUser.join(roomId);
 
-            // first user caller
             waitingUser.emit("matched", {
                 roomId,
                 isCaller: true
             });
 
-            // second user
             socket.emit("matched", {
                 roomId,
                 isCaller: false
@@ -48,33 +42,25 @@ io.on("connection", (socket) => {
         } else {
 
             waitingUser = socket;
-
             socket.emit("waiting");
         }
     });
 
-    // 📤 Offer
     socket.on("offer", (data) => {
-
         socket.to(data.roomId)
             .emit("offer", data.offer);
     });
 
-    // 📤 Answer
     socket.on("answer", (data) => {
-
         socket.to(data.roomId)
             .emit("answer", data.answer);
     });
 
-    // 📤 ICE
     socket.on("ice-candidate", (data) => {
-
         socket.to(data.roomId)
             .emit("ice-candidate", data.candidate);
     });
 
-    // ⏭️ Next User
     socket.on("next", () => {
 
         socket.rooms.forEach(room => {
@@ -90,33 +76,13 @@ io.on("connection", (socket) => {
 
         if (waitingUser &&
             waitingUser.id === socket.id) {
-
             waitingUser = null;
         }
 
         socket.emit("waiting");
     });
-
-    // ❌ Disconnect
-    socket.on("disconnect", () => {
-
-        if (waitingUser &&
-            waitingUser.id === socket.id) {
-
-            waitingUser = null;
-        }
-
-        socket.rooms.forEach(room => {
-
-            socket.to(room)
-                .emit("partner-disconnected");
-        });
-
-        console.log("Disconnected:", socket.id);
-    });
 });
 
 server.listen(process.env.PORT || 3000, () => {
-
     console.log("Server running");
 });
